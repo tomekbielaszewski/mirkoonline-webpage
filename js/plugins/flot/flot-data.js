2fd95739e0320274b442574eee5d8c71a175d263
Grizwold <tomekbielaszewski@gmail.com>
2017-01-03 23:55:07 +0100

    latest version

--- js/plugins/flot/flot-data.js
+++ js/plugins/flot/flot-data.js
@@ -1,63 +1,68 @@
 $(function () {
+  function updateGraph() {
     var dayAgo = new Date().getTime() - (1000 * 60 * 60 * 48);
     $.ajax({
-        url: "http://46.101.0.193:8080/entries/mirkoonline/" + dayAgo,
-        dataType: "jsonp",
-        success: function (mirkoonlineData) {
-            var formattedData = [];
+      url: "http://35.160.50.195:8080/entries/mirkoonline/" + dayAgo,
+      dataType: "jsonp",
+      success: function (mirkoonlineData) {
+        var formattedData = [];
 
-            $.each(mirkoonlineData, function (index, entry) {
-                formattedData.push([entry.date, parseInt(entry.value)]);
-            });
+        $.each(mirkoonlineData, function (index, entry) {
+          formattedData.push([entry.date, parseInt(entry.value)]);
+        });
 
-            function doPlot() {
-                $.plot($("#flot-multiple-axes-chart"), [{
-                    data: formattedData,
-                    label: "Aktywnych"
-                }, {
-                    yaxis: 2
-                }], {
-                    xaxes: [{
-                        mode: 'time'
-                    }],
-                    yaxes: [{
-                        min: 0
-                    }, {
-                        alignTicksWithAxis: 1,
-                        position: "right"
-                    }],
-                    legend: {
-                        position: 'sw'
-                    },
-                    grid: {
-                        hoverable: true //IMPORTANT! this is needed for tooltip to work
-                    },
-                    tooltip: true,
-                    tooltipOpts: {
-                        content: "Data: %x;Aktywnych:%y",
-                        xDateFormat: "%y-%0m-%0d %0h:%0M",
-
-                        onHover: function (flotItem, $tooltipEl) {
-                            // console.log(flotItem, $tooltipEl);
-                        }
-                    }
-                });
+        function doPlot() {
+          $.plot($("#flot-multiple-axes-chart"), [{
+            data: formattedData,
+            label: "Aktywnych"
+          }, {
+            yaxis: 2
+          }], {
+            xaxes: [{
+              mode: 'time',
+              timezone: 'browser'
+            }],
+            yaxes: [{
+              min: 0
+            }, {
+              alignTicksWithAxis: 1,
+              position: "right"
+            }],
+            legend: {
+              position: 'sw'
+            },
+            grid: {
+              hoverable: true //IMPORTANT! this is needed for tooltip to work
+            },
+            tooltip: true,
+            tooltipOpts: {
+              content: "Data: %x;Aktywnych:%y",
+              xDateFormat: "%y-%0m-%0d %0h:%0M",
             }
-
-            doPlot();
+          });
         }
-    });
-});
 
-$(function () {
+        doPlot();
+      }
+    });
+  }
+
+  function updateHeader() {
     $.ajax({
-        url: 'http://46.101.0.193:8080/entries/last/mirkoonline',
-        dataType: 'jsonp',
-        success: function (data) {
-            $('#active_users').text(data.value);
-        },
-        error: function () {
-            $('#active_users').text('Bot niedostepny... ;___;');
-        }
+      url: 'http://35.160.50.195:8080/entries/last/mirkoonline',
+      dataType: 'jsonp',
+      success: function (data) {
+        $('#active_users').text(data.value);
+      },
+      error: function () {
+        $('#active_users').text('Bot niedostepny... ;___;');
+      }
     });
+  }
+
+  updateGraph();
+  updateHeader();
+
+  setInterval(updateGraph, 300000);
+  setInterval(updateHeader, 300000);
 });
